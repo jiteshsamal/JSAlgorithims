5216d6882bc37a8fddef88f0fb756d29febacaad
jitesh samal <jitesh.samal@gmail.com>
2018-07-30 12:44:17 -0400

    Add files via upload

---
+++ swapNumbers.js
@@ -0,0 +1,8 @@
+
+function swapNumb(a, b){
+    console.log('before swap: ','a: ', a, 'b: ', b);
+    b = b -a;
+    a = a+ b;
+    b = a-b;
+    console.log('after swap: ','a: ', a, 'b: ', b);  
+  }
